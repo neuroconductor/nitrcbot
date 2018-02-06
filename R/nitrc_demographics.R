f4f0861cee815ffa87b7193c989c00a980ac2df8
adigherman <adig@jhu.edu>
2018-02-06 15:53:47 -0500

    returning scan data as data.frame

--- R/nitrc_demographics.R
+++ R/nitrc_demographics.R
@@ -4,6 +4,7 @@
 #'  we request demographics data, if project
 #'  is NULL, we return all available subjects
 #'
+#' @importFrom dplyr bind_rows
 #' @return Dataframe of demographics data
 #' @export
 nitrc_demographics = function(
@@ -14,22 +15,19 @@ nitrc_demographics = function(
 
   if(!is.null(nitrc_projects)){
     if(!is.null(project)){
+      demographics_content = NULL
       if(project %in% nitrc_projects$ID){
-        demographics_content <- content(GET(paste0("https://www.nitrc.org/ir/data/subjects?columns=label,gender,handedness,project&project=",project,"&format=json")))
-        demographics = NULL
-        for(i in 1:length(demographics_content$ResultSet$Result)){
-          demographics = rbind(demographics,data.frame(demographics_content$ResultSet$Result[[i]]$ID,demographics_content$ResultSet$Result[[i]]$label,demographics_content$ResultSet$Result[[i]]$project,demographics_content$ResultSet$Result[[i]]$gender,demographics_content$ResultSet$Result[[i]]$handedness),stringsAsFactors = FALSE)
-        }
-        colnames(demographics) <- c("ID","Label","Project","Gender","Handedness")
-        return(demographics)
+        demographics_content = content(GET(paste0("https://www.nitrc.org/ir/data/subjects?columns=label,gender,handedness,project&project=",project,"&format=json")))
       }
       else{
         return(message(paste0('Could not find project ',project,' in NITRC')))
       }
     }
     else{
-      return(content(GET("https://www.nitrc.org/ir/data/subjects?columns=label,gender,handedness,project&format=json")))
+      demographics_content = content(GET("https://www.nitrc.org/ir/data/subjects?columns=label,gender,handedness,project&format=json"))
     }
+    demographics = bind_rows(lapply(demographics_content$ResultSet$Result, as.data.frame, stringsAsFactors = FALSE))
+    return(demographics)
   }
   return(nitrc_projects)
 }
--- R/nitrc_scandata.R
+++ R/nitrc_scandata.R
@@ -13,17 +13,19 @@ nitrc_scandata = function(
 
   if(!is.null(nitrc_projects)){
     if(!is.null(project)){
+      scan_content = NULL
       if(project %in% nitrc_projects$ID){
-        scan_content <- content(GET(paste0("https://www.nitrc.org/ir/data/experiments?columns=xnat:mrSessionData/ID,label,,xnat:imageScanData/type,xnat:imageScanData/ID,ID,label,subject_ID,,subject_label,xnat:mrScanData/parameters/frames,xnat:mrScanData/parameters/te,xnat:mrScanData/parameters/tr&project=",project,"&format=json")))
-        return(scan_content)
+        scan_content <- content(GET(paste0("https://www.nitrc.org/ir/data/experiments?columns=xnat:mrSessionData/ID,label,,xnat:imageScanData/type,xnat:imageScanData/ID,ID,label,subject_ID,,subject_label,xnat:mrScanData/parameters/frames,xnat:mrScanData/parameters/te,xnat:mrScanData/parameters/tr,xnat:mrScanData/parameters/flip,xnat:mrScanData/parameters/voxelRes/x,xnat:mrScanData/parameters/voxelRes/y,xnat:mrScanData/parameters/voxelRes/z,xnat:mrScanData/fieldStrength,xnat:mrScanData/parameters/matrix/x,xnat:mrScanData/parameters/matrix/y,xnat:mrScanData/parameters/partitions,xnat:mrScanData/quality,xnat:mrSessionData/age&project=",project,"&format=json")))
       }
       else{
         return(message(paste0('Could not find project ',project,' in NITRC')))
       }
     }
     else{
-      return(message("coming soon..."))
+      scan_content <- content(GET(paste0("https://www.nitrc.org/ir/data/experiments?columns=xnat:mrSessionData/ID,label,,xnat:imageScanData/type,xnat:imageScanData/ID,ID,label,subject_ID,,subject_label,xnat:mrScanData/parameters/frames,xnat:mrScanData/parameters/te,xnat:mrScanData/parameters/tr,xnat:mrScanData/parameters/flip,xnat:mrScanData/parameters/voxelRes/x,xnat:mrScanData/parameters/voxelRes/y,xnat:mrScanData/parameters/voxelRes/z,xnat:mrScanData/fieldStrength,xnat:mrScanData/parameters/matrix/x,xnat:mrScanData/parameters/matrix/y,xnat:mrScanData/parameters/partitions,xnat:mrScanData/quality,xnat:mrSessionData/age&format=json")))
     }
+    scandata = bind_rows(lapply(scan_content$ResultSet$Result, as.data.frame, stringsAsFactors = FALSE))
+    return(scandata)
   }
   else{
     return(nitrc_projects)
