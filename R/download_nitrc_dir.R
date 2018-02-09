233c8ebbb99e571f47ae1203440e508f0fba4206
adigherman <adig@jhu.edu>
2018-02-09 16:57:46 -0500

    added file prefix to download_nitrc_file

--- R/download_nitrc_dir.R
+++ R/download_nitrc_dir.R
@@ -1,6 +1,20 @@
 #' @title Download image(s)
-#' @description Downloads whole scan folders or individual images
+#' @description Download entire session or just the files from a particular
+#' scan type. If \code{scan_type} is NULL the function will download a zipped
+#' file containing the entire resource folder for that particular \code{subject_ID}
+#' or \code{session_ID}. If \code{scan_type} is specified than depending on the
+#' \code{zipped} the downloaded files will be zipped or not.
 #' @param subject_ID Download scan images for this particular subject ID
 #' @param session_ID Downoad scan images for this particular session ID
 #' @param scan_type Download scan images just for this particular type of scan
-#' @param file_name Download just this file
+#' @param zipped Zip the downloaded files
+#'
+#' @return List of downloaded file(s) with full paths
+download_nitrc_dir = function(subject_ID,
+                              session_ID = NULL,
+                              scan_type = NULL,
+                              zipped = FALSE) {
+
+
+}
+
--- R/download_nitrc_file.R
+++ R/download_nitrc_file.R
@@ -2,20 +2,25 @@
 #' @description Download a single file from NITRC
 #' @param file_path Path to the file to be dowloaded
 #' @param destfile Destination filename
+#' @param prefix Prefix the file name with this
 #' @param verbose Should progress be added to download?
 #' @param error Should function error if download failed?
 #'
 #' @return Display path to the downloaded file
-#' @importFrom httr stop_for_status write_disk progress
+#' @importFrom httr stop_for_status write_disk progress GET
 #' @export
 #' @examples \dontrun{download_nitrc_file('/data/experiments/NITRC_IR_E11081/scans/B0_1000_2000_raw_acq2/resources/83962/files/dki_2000_18.nii.gz',verbose=TRUE)}
 download_nitrc_file = function(file_path,
                                destfile = NULL,
+                               prefix = NULL,
                                verbose = FALSE,
                                error = FALSE
                                ) {
   if (is.null(destfile)) {
-      destfile = file.path(tempdir(),basename(file_path))
+      if(!is.null(prefix)) {
+        prefix <- paste0(prefix,"_")
+      }
+      destfile = file.path(tempdir(),paste0(prefix,basename(file_path)))
   }
   args = list(
     url = paste0("https://www.nitrc.org/ir",file_path),
