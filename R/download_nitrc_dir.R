b78d7c52356e310a75ecf190cf96afd0f17c7859
adigherman <adig@jhu.edu>
2018-02-14 11:55:48 -0500

    return tempdir rather than tempfile

--- R/download_nitrc_dir.R
+++ R/download_nitrc_dir.R
@@ -86,7 +86,7 @@ download_nitrc_dir = function(session_ID,
       }
       download_nitrc_file(file_path = file, prefix = prefix)
     }, scan_resources[, "URI"], scan_resources[, "cat_ID"])
-    file_list <- list(outdir = tempfile(),
+    file_list <- list(outdir = tempdir(),
         files = paste0(scan_resources[, "cat_ID"],"_",basename(scan_resources[, "URI"])))
     return(file_list)
 
