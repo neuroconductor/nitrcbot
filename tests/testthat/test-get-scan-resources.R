4b9f762f5841804b1fbe28e5a3a5b29ff18247c0
adigherman <adig@jhu.edu>
2018-02-16 11:35:32 -0500

    fixed get_scan_resources tests

--- tests/testthat/test-get-scan-resources.R
+++ tests/testthat/test-get-scan-resources.R
@@ -2,7 +2,7 @@ test_that("get_scan_resources", {
   set_credentials(username = "fakeuser", password = "fakepassword")
 
   testthat::expect_is(get_scan_resources('NITRC_IR_E10453'),"data.frame")
-  testthat::expect_message(get_scan_params('NITRC_IR_E10453',scan_type = 'T3'),"Invalid scan type")
-  testthat::expect_is(get_scan_params('NITRC_IR_E10453',scan_type = 'T1'), "data.frame")
+  testthat::expect_message(get_scan_resources('NITRC_IR_E10453',scan_type = 'T3'),"Invalid scan type")
+  testthat::expect_is(get_scan_resources('NITRC_IR_E10453',scan_type = 'T1'), "data.frame")
   testthat::expect_null(get_scan_resources('NITRC_IR_E11072'))
 })
